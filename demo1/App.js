e9beb6682ea081891c4f2957811cdb2f596590c5
LA - TA MINH KHA <tmk001-dn-la@fhmc.com>
2020-10-14 09:48:56 +0700

    lần 3

--- demo1/App.js
+++ demo1/App.js
@@ -1,47 +1,7 @@
-// import { StatusBar } from 'expo-status-bar';
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-//
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.js to start working on your app!</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-//
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+
 import React from "react";
-import { Button, Platform, Text, Vibration, View, SafeAreaView, StyleSheet,StatusBar } from "react-native";
+import { Button, Platform, Text, Vibration, View, SafeAreaView, StyleSheet,StatusBar,Image } from "react-native";
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
-const App = () => {
-  return (
-
-      <View>
-        <StatusBar barStyle="ligh-content"/>
-          <View style={styles.headerContainer}>
-              <View style={styles.inputContainer}>
-                  <FontAwesome name="search" size={30}/>
-                <Text>Bạn tìm gì hôm nay?</Text>
-              </View>
-              <View style={styles.cartContainer}>
-                  <FontAwesome name="shopping-cart" size={24}/>
-              </View>
-
-          </View>
-      </View>
-
-  );
-}
-
 const styles = StyleSheet.create({
   headerContainer: {
     flexDirection: 'row',
@@ -59,26 +19,77 @@ const styles = StyleSheet.create({
     paddingHorizontal:12,
     paddingVertical:8,
   },
+  inputText:{
+    color:'#969696',
+    fontSize:14,
+    marginLeft:8,
+    fontWeight:'500'
+  },
   cartContainer:{
     flexDirection: 'row',
     paddingHorizontal:20,
     justifyContent:'center',
     alignItems:'center'
   },
+  bodyContainer:{
+    backgroundColor:'#fff'
+  },
+  sectionContainer:{
+    backgroundColor:'#fff'
+  },
+  sectionTitle:{
+    fontWeight:'700',
+    fontSize:16,
+    color:'#2f2f2f'
+  },
+
   header: {
     fontSize: 18,
     fontWeight: "bold",
     textAlign: "center"
   },
-  paragraph: {
-    margin: 24,
-    textAlign: "center"
-  },
+
   separator: {
     marginVertical: 8,
     borderBottomColor: "#737373",
     borderBottomWidth: StyleSheet.hairlineWidth
   }
 });
+const ProducItem=({image,name,price})=>(
+  <View style={styles.itemContainer}>
+    <Image source={image} style={styles.itemImages}/>
+
+  </View>
+)
+const App = () => {
+  return (
+
+      <View>
+        <StatusBar barStyle="ligh-content"/>
+          <View style={styles.headerContainer}>
+              <View style={styles.inputContainer}>
+                  <FontAwesome name="search" size={30}/>
+                <Text>Bạn tìm gì hôm nay?</Text>
+              </View>
+              <View style={styles.cartContainer}>
+                  <FontAwesome name="shopping-cart" size={24}/>
+              </View>
+          </View>
+          <View style={styles.bodyContainer}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Điện thoại - Máy tính bảng</Text>
+      
+            </View>
+            <View style={styles.listItemContainer}>
+              <View style={styles.itemContainer}>
+                <Image source={require('./src/assets/section_banner.png')} />
+              </View>
+            </View>
+          </View>
+      </View>
+
+  );
+}
+
 
 export default App;
